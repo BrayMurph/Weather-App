c84925479e3ba8430143b901deff342af5884db0
BrayMurph <130401538+BrayMurph@users.noreply.github.com>
2023-10-05 06:49:14 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
+var apikey = "";
+var today = moment().format("L");
+var searchHistory = [];
 
+function currentCond(city) {
+    var queryURL = "";
+    
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function(cityWeatherRes) {
+          console.log(cityWeatherRes);
+          $("#weatherCont").css("display", "block");
+          $("#cityDetail").empty();
+
+          var iconCode = cityWeatherRes.weather[0].icon;
+          var iconIMG = `https://openweathermap.org/img/w/${iconCode}.png`;
+          var currentCity = $(`
+            <h2 id="currentCity">
+                ${cityWeatherRes.name} ${today} <img src="${iconIMG}" alt="${cityWeatherRes.weather[0].description}" />
+            </h2>
+            <p>Temperature: ${cityWeatherRes.main.temp} °F</p>
+            <p>Humidity: ${cityWeatherRes.main.humidity}\%</p>
+            <p>Wind Speed: ${cityWeatherRes.wind.speed} MPH</p>
+          `);
+          $("#cityDetail").append(currentCity);
+
+          var 
+    }
+}
