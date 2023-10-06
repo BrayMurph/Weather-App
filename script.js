5cef77b91a49f7ba909a1c363cb1eafe32255fa2
BrayMurph <130401538+BrayMurph@users.noreply.github.com>
2023-10-06 06:30:18 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 var apikey = "";
 var today = moment().format("L");
-var searchHistory = [];
+var searchList = [];
 
 function currentCond(city) {
-    var queryURL = "";
+    var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
     
     $.ajax({
       url: queryURL,
@@ -25,6 +25,108 @@ function currentCond(city) {
           `);
           $("#cityDetail").append(currentCity);
 
-          var 
-    }
+          var lat = cityWeatherRes.coord.lat;
+          var lon = cityWeatherRes.coord.lon;
+          var uviqueryURL = `https://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+
+        $.ajax({
+            url: uviqueryURL,
+            method: "GET"
+        }).then(function(uviRes) {
+            var uvIndex = uviRes.value;
+            var uvIndexP = $(`
+                <p>UV Index:
+                    <span id="uvIndexColor" class="px-2 py-2 rounded">${uvIndex}</span>
+                </p>
+                `);
+
+            $("#cityDetail").append(uvIndexP);
+
+            fiveday(lat, lon);
+
+            if (uvIndex >= 0 && uvIndex <= 2) {
+                $("#uvIndexColor").css("background-color", "#3EA72D").css("color", "white");
+            } else if (uvIndex >= 3 && uvIndex <= 5) {
+                $("#uvIndexColor").css("background-color", "#FFF300");
+            } else if (uvIndex >= 6 && uvIndex <= 7) {
+                $("#uvIndexColor").css("background-color", "#F18B00");
+            } else if (uvIndex >= 8 && uvIndex <= 10) {
+                $("#uvIndexColor").css("background-color", "#E53210").css("color", "white");
+            } else {
+                $("#uvIndexColor").css("background-color", "#B567A4").css("color", "white");
+            };         
+        });
+    });
 }
+
+function fiveday(lat, lon) {
+    var fivedayURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=current,minutely,hourly,alerts&appid=${apiKey}`;
+
+    $.ajax({
+        url: fivedayURL,
+        method: "GET"
+    }).then(function(fivedayRes) {
+        $("#fiveDay").empty();
+
+        for (let i = 1; i < 6; i++) {
+            var cityData = {
+                date: fivedayRes.daily[i].dt,
+                icon: fivedayRes.daily[i].weather[0].icon,
+                temp: fivedayRes.daily[i].temp.day,
+                humidity: fivedayRes.daily[i].humidity
+            };
+
+            var currentDate = moment.unix(cityData.date).format("MM/DD/YYYY");
+            var iconIMG = `<img src="https://openweathermap.org/img/w/${cityData.icon}.png" alt="${fivedayRes.daily[i].weather[0].main}" />`;
+
+            var fivedayCard = $(`
+                <div class="pl-3">
+                    <div class="card pl-3 pt-3 mb-3 bg-primary text-light" style="width: 12rem">
+                        <div class="card-body">
+                            <h5>${currentDate}</h5>
+                            <p>${iconIMG}</p>
+                            <p>Temperature: ${cityData.temp} °F</p>
+                            <p>Humidity: ${cityData.humidity}\%</p>
+                        </div>
+                    </div>
+                </div>
+            `);
+
+            $("#fiveDay").append(fivedayCard);
+        }
+    });
+}
+
+        $("#searchBtn").on("click", function(event) {
+            event.preventDefault();
+
+            var city = $("#inputCity").val().trim();
+            currentCond(city);
+            if (!searchList.includes(city)) {
+                searchList.push(city);
+                searchedCity = $(`
+                    <li class="list-item">${city}</li>
+                    `);
+                $("#searchHistory").append(searchedCity);
+            };
+
+            localStorage.setItem("city", JSON.stringify(searchList));
+        });        
+
+        $(document).on("click", ".list-item", function() {
+            var listCity = $(this).text();
+            currentCond(listCity);
+        });
+
+        $(document).ready(function() {
+            var searchArr = JSON.parse(localStorage.getItem(city));
+
+            if (searchArr !== null) {
+                var lastSearched = searchArr.length - 1;
+                var lastCity = searchArr[lastSearched];
+                currentCond(lastCity);
+            }
+        });
+
+        
+
